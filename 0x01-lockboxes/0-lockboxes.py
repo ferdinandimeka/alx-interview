fc51101da1310a4471165f660a7cc486666e8085
unknown <ferdinandimeka20@gmail.com>
2023-07-31 22:11:17 +0100

    update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,15 +5,20 @@ A method that determines if all the boxes can be opened
 
 from collections import deque
 
+
 def canUnlockAll(boxes):
     n = len(boxes)
     visited = [False] * n
     visited[0] = True
-    queue = deque([0]) # start with the first box
+
+    # start with the first box
+    queue = deque([0])
+
     '''
-    perform BFS by popping a box from the queue, checking its keys, and adding 
+    perform BFS by popping a box from the queue, checking its keys, and adding\
     unvisited keys to the queue while marking them as visited.
     '''
+
     while queue:
         curr_box = queue.popleft()
         for key in boxes[curr_box]:
@@ -22,4 +27,3 @@ def canUnlockAll(boxes):
                 queue.append(key)
 
     return all(visited)
-
