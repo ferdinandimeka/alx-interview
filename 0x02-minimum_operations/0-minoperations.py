1c03c0057ebb34f1f9ee0d1c697af51ac3167333
root <root@ferdinandimeka>
2023-08-07 23:10:58 +0100

    minOperations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+    module that calculates the fewest number of operations needed
+    to result in exactly n H characters in the file.
+"""
+
+
+def minOperations(n: int) -> int:
+    """
+        calculates the fewest number of operations
+    """
+    if n == 1:
+        return 0 # base case: already have 1 H
+    
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+    return operations
