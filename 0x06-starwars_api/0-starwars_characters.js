248e5f54a0bdc578b9b5abff7067a0848af78d54
root <root@ferdinandimeka>
2023-09-05 12:19:03 +0100

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,25 +3,25 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/';
 
 if (process.argv.length > 2) {
-    request(`${url}films/${process.argv[2]}/`, (err, res, body) => {
-        if (err) {
-            console.log(err);
-        }
+  request(`${url}films/${process.argv[2]}/`, (err, res, body) => {
+    if (err) {
+      console.log(err);
+    }
 
-        const characters = JSON.parse(body).characters;
-        const characterName = characters.map((character) => {
-            return new Promise((resolve, reject) => {
-                request(character, (err, res, body) => {
-                    if (err) {
-                        reject(err);
-                    }
-                    resolve(JSON.parse(body).name);
-                });
-            });
+    const characters = JSON.parse(body).characters;
+    const characterName = characters.map((character) => {
+      return new Promise((resolve, reject) => {
+        request(character, (err, res, body) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(JSON.parse(body).name);
         });
-
-        Promise.all(characterName)
-            .then(names => console.log(names.join('\n')))
-            .catch(err => console.log(err))
+      });
     });
+
+    Promise.all(characterName)
+      .then(names => console.log(names.join('\n')))
+      .catch(err => console.log(err));
+  });
 }
